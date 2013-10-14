1cefa8dbeeaf035f5a261876390d909f9d24cb28
Erik Kroon <mail@punchcreative.nl>
2013-10-14 19:11:39 +0200

    added GPL2

--- get-your-number.php
+++ get-your-number.php
@@ -7,14 +7,22 @@
  * Author: Erik Kroon | Punch Creative
  * Author URI: http://www.punchcreative.nl
  * License: GPL2
- * Copyright 2013  Erik Kroon
+ Copyright 2013  Erik Kroon  (email : mail@punchcreative.nl)
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License, version 2, as 
-   published by the Free Software Foundation.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+$dir = plugin_dir_path( __FILE__ );
+
+?>
